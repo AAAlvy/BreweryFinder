f8bfccde96b05de5884f93673fc8cc5d1d264a63
AAAlvy <alvarezandrewa@gmail.com>
2017-05-18 09:37:14 -0600

    added hyperlink, and some style changes

---
+++ mdFiles/README-template.md
@@ -0,0 +1,32 @@
+# Q1 Project Proposal
+# Andrew's Q1 Project Proposal (working title pending)
+  (Which Brewery?, Where Beer?, Get Our
+    Brewery On, Qual Pivovar?)
+
+## Project Description
+  A site/app for helping decide which brewery to visit based on a few key factors.
+
+## What problem does your project solve?
+  It solves the need for a brewery-specific (as a whole: beer, location, food availability, atmosphere) rating app.
+
+## Who has this problem?
+  Beer and brewery enthusiasts.
+
+## How will your project solve this problem?
+  By boiling down beer ratings, location services, kitchen/food truck lists, and other rating factors, and blending them into one search tool.
+
+## What inputs does it need?
+  The user will need to input which of the search criteria of their present location.
+
+## What outputs does it produce?
+  It will produce a list of breweries that best match their criteria.     
+
+## What web API(s) will it use?
+  It will use the APIs of Brewery DB or Untappd).
+
+## What technologies do you plan to use?
+  Technologies employed will include: HTML, CSS, JavaScript, and Materialize.
+
+## Feature list
+  Features on the app will include: a landing page with buttons that allow you to determine what kind of search you want to conduct. These buttons will be labeled and lead to appropriate pages, such as: search by name, quick location search, or search by rating factors. Based upon which search option the user chooses, a feature page of "results" will be produced.
+  User stories:
--- script.js
+++ script.js
@@ -12,25 +12,26 @@ $("#submit").click(function(){
   $.getJSON('https://beer-search.now.sh/?lat=39.7392358&lng=-104.990251&radius=' + radius).then(function(results){
     console.log(results)
     for(var i=0; i < results.data.length; i++){
-      var breweryNames = "<p>" + results.data[i].brewery.name + " latitude " + results.data[i].latitude + " longitude " + results.data[i].longitude + "</p>";
+      var breweryNames = "<p>" + results.data[i].brewery.name + ", " + results.data[i].website + "</p>";
       $(".results").append(breweryNames)
     }
   })
   $(".results").empty(); //This will be search function #1, find breweries by ****** original one sent by CJ
 
 });
+// var breweryNames = "<p>" + results.data[i].brewery.name + ", latitude " + results.data[i].latitude + " longitude " + results.data[i].longitude + "</p>"
 
-})
-$("#submit").click(function(){
-  event.preventDefault();
-  var radius = $("#drinking_radius").val()
-  console.log(radius)
-  $.getJSON('https://beer-search.now.sh/get-breweries?lat=39.7392358&lng=-104.990251&radius=' + radius).then(function(results){
-    console.log(results)
-    for(var i=0; i < results.data.length; i++){
-      var breweryNames = "<p>" + results.data[i].brewery.name + " latitude " + results.data[i].latitude + " longitude " + results.data[i].longitude + "</p>";
-      $(".results").append(breweryNames)
-    }
-  })
-  $(".results").empty();
+// })
+// $("#submit").click(function(){
+//   event.preventDefault();
+//   var radius = $("#drinking_radius").val()
+//   console.log(radius)
+//   $.getJSON('https://beer-search.now.sh/get-breweries?lat=39.7392358&lng=-104.990251&radius=' + radius).then(function(results){
+//     console.log(results)
+//     for(var i=0; i < results.data.length; i++){
+//       var breweryNames = "<p>" + results.data[i].brewery.name + " latitude " + results.data[i].latitude + " longitude " + results.data[i].longitude + "</p>";
+//       $(".results").append(breweryNames)
+//     }
+//   })
+//   $(".results").empty();
 });
